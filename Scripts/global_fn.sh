3f1af569338337ebae157738884ef27d93129844
Roberto Flores <25asab015@ujmd.edu.sv>
2026-05-27 21:02:25 -0600

    feat(global_fn): add missing pkg_available, aur_available, and print_log functions

--- Scripts/global_fn.sh
+++ Scripts/global_fn.sh
@@ -60,6 +60,27 @@ chk_list() {
     return 1
 }
 
+pkg_available() {
+    local PkgIn=$1
+
+    if ${pacmanCmd} query "${PkgIn}" &>/dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+aur_available() {
+    local PkgIn=$1
+
+    # shellcheck disable=SC2154
+    if ${pacmanCmd} info "${PkgIn}" &>/dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 # Detecta adaptadores de gráficos (GPU) de Nvidia en el sistema.
 # Opciones:
 #   --verbose : Imprime en consola todas las GPUs detectadas con su índice.
@@ -109,4 +130,82 @@ prompt_timer() {
     export PROMPT_INPUT
     echo ""
     set -e
+}
+print_log() {
+    local executable="${0##*/}"
+    local logFile="${cacheDir}/logs/${RAVN_LOG}/${executable}.log"
+    mkdir -p "$(dirname "${logFile}")"
+    local section=${log_section:-}
+    {
+        [ -n "${section}" ] && echo -ne "\e[32m[$section] \e[0m"
+        while (("$#")); do
+            case "$1" in
+            -r | +r)
+                echo -ne "\e[31m$2\e[0m"
+                shift 2
+                ;; # Red
+            -g | +g)
+                echo -ne "\e[32m$2\e[0m"
+                shift 2
+                ;; # Green
+            -y | +y)
+                echo -ne "\e[33m$2\e[0m"
+                shift 2
+                ;; # Yellow
+            -b | +b)
+                echo -ne "\e[34m$2\e[0m"
+                shift 2
+                ;; # Blue
+            -m | +m)
+                echo -ne "\e[35m$2\e[0m"
+                shift 2
+                ;; # Magenta
+            -c | +c)
+                echo -ne "\e[36m$2\e[0m"
+                shift 2
+                ;; # Cyan
+            -wt | +w)
+                echo -ne "\e[37m$2\e[0m"
+                shift 2
+                ;; # White
+            -n | +n)
+                echo -ne "\e[96m$2\e[0m"
+                shift 2
+                ;; # Neon
+            -stat)
+                echo -ne "\e[30;46m $2 \e[0m :: "
+                shift 2
+                ;; # status
+            -crit)
+                echo -ne "\e[97;41m $2 \e[0m :: "
+                shift 2
+                ;; # critical
+            -warn)
+                echo -ne "WARNING :: \e[30;43m $2 \e[0m :: "
+                shift 2
+                ;; # warning
+            +)
+                echo -ne "\e[38;5;$2m$3\e[0m"
+                shift 3
+                ;; # Set color manually
+            -sec)
+                echo -ne "\e[32m[$2] \e[0m"
+                shift 2
+                ;; # section use for logs
+            -err)
+                echo -ne "ERROR :: \e[4;31m$2 \e[0m"
+                shift 2
+                ;; #error
+            *)
+                echo -ne "$1"
+                shift
+                ;;
+            esac
+        done
+        echo ""
+    } | if [ -n "${RAVN_LOG}" ]; then
+        tee >(sed 's/\x1b\[[0-9;]*m//g' >>"${logFile}")
+    else
+        cat
+    fi
 }
